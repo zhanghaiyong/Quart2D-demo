980b7501c7bc9173221dbda9b4a186cf83c66ec5
zhanghaiyong <973490661@qq.com>
2017-08-02 11:24:40 +0800

    创建工程

---
+++ quart2D-demo/quart2D-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quart2D-demo
+//
+//  Created by zhy on 2017/8/2.
+//  Copyright © 2017年 zhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ quart2D-demo/quart2D-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quart2D-demo
+//
+//  Created by zhy on 2017/8/2.
+//  Copyright © 2017年 zhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
